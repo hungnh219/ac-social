49c61185103385c542dd32439f378cb58c813209
Tú Nguyễn <ntu4318@gmail.com>
2024-10-09 04:03:08 +0700

    Pull from release branch

---
+++ lib/services/firebase_auth_service.dart
@@ -0,0 +1,90 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+const defaultAvatarUrl = "https://firebasestorage.googleapis.com/v0/b/ac-social-internship.appspot.com/o/default_avatar.png?alt=media&token=822ddf23-8cf3-434e-87e3-81fd35491e84";
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleAuthProvider _googleProvider = GoogleAuthProvider();
+
+  Stream<User?> get userStream => _auth.authStateChanges();
+
+  User? get user => _auth.currentUser;
+
+  Future<void> signInWithEmailAndPassword(String email, String password) async {
+    try {
+      await _auth.signInWithEmailAndPassword(
+          email: email.trim(), password: password);
+
+      User user = _auth.currentUser!;
+
+      // If signed up user not confirm the verify link sent via through gmail
+      if (!user.emailVerified) {
+        signOut();
+        throw FirebaseAuthException(
+          code: 'email-not-verified',
+          message: 'Your account is not verified. Please check your inbox',
+        );
+      }
+    } catch (error) {
+      if (kDebugMode) {
+        print(error.toString());
+      }
+
+      rethrow;
+    }
+  }
+
+  Future<void> signInWithGoogle() async {
+    try {
+      if (kIsWeb) {
+        _auth.signInWithPopup(_googleProvider);
+      } else {
+        final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+        final GoogleSignInAuthentication googleAuth =
+        await googleUser!.authentication;
+
+        // Create a GoogleAuthProvider credential
+        final AuthCredential credential = GoogleAuthProvider.credential(
+          accessToken: googleAuth.accessToken,
+          idToken: googleAuth.idToken,
+        );
+
+        // Sign in to Firebase with Google credentials
+        await _auth.signInWithCredential(credential);
+      }
+    } catch (error) {
+      if (kDebugMode) {
+        print(error.toString());
+      }
+    }
+  }
+
+  Future<void> signUpWithEmailAndPassword(String username, String email,
+      String password, String confirmPassword) async {
+    try {
+      UserCredential userCredential =
+      await _auth.createUserWithEmailAndPassword(
+        email: email.trim(),
+        password: password,
+      );
+
+      await userCredential.user!.sendEmailVerification();
+      await userCredential.user!.updateDisplayName(username);
+      await userCredential.user!.updatePhotoURL(defaultAvatarUrl);
+      // await userCredential.user!.reload();
+      signOut();
+    } catch (error) {
+      if (kDebugMode) {
+        print(error.toString());
+      }
+
+      rethrow;
+    }
+  }
+
+  Future<void> signOut() async {
+    await _auth.signOut();
+  }
+}
