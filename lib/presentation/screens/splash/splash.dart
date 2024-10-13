d80bc8b2b675c2504921ca796a514ec6b48fd7ed
Huong Truong <truongthihuong20020110@gmail.com>
2024-10-13 19:44:28 +0700

    splash screen and boarding screen

---
+++ lib/presentation/screens/splash/splash.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:social_app/presentation/widgets/splash/splash_background.dart';
+import 'package:social_app/presentation/widgets/splash/splash_image.dart';
+import 'package:social_app/presentation/widgets/splash/splash_image_group.dart';
+import 'package:social_app/utils/constants/image_path.dart';
+import 'package:social_app/utils/styles/colors.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    initialization();
+  }
+
+  void initialization() async {
+    await Future.delayed(const Duration(seconds: 5));
+    context.go("/boarding");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const SplashBackground(
+      center: Center(
+        child: SplashImageGroup(),
+      ),
+    );
+  }
+}
