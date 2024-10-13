7147b94d9d170eec6877ddd9721688abba108753
Tú Nguyễn <ntu4318@gmail.com>
2024-10-14 01:39:46 +0700

    Save currents code changes

---
+++ lib/main_dev.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:social_app/main.dart';
+import 'package:social_app/presentation/screens/setting/setting_screen.dart';
+import 'package:social_app/utils/styles/themes.dart';
+
+void main(){
+  runApp(MyTestApp());
+}
+
+class MyTestApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: AppTheme.lightTheme,
+      debugShowCheckedModeBanner: false,
+      home: CustomDrawerPage(),
+    );
+  }
+
+}
