0312114c5bc5f6c5d20da4a996df375df69471ee
Huong Truong <truongthihuong20020110@gmail.com>
2024-10-13 19:56:26 +0700

    features and screens of sign in and sign up

--- lib/mixin/validators/validators.dart
+++ lib/mixin/validators/validators.dart
@@ -11,29 +11,51 @@ mixin Validator {
   String? validateUsername(String? value) {
     if (value == null || value.isEmpty) {
       return 'Please enter a your username';
-    } else if (value.length > 20){
+    } else if (value.length > 20) {
       return 'Your username is too long';
-    }
-    else if(value.contains(" ")){
+    } else if (value.contains(" ")) {
       return 'You username must not contain spaces';
     }
     return null;
   }
 
-  String? validateEmail(String? value) {
-    if (value!.isEmpty || !EmailValidator.validate(value)) {
+  String? validateEmail(String value) {
+    if (validateEmpty(value)) {
+      return "Please enter a your email";
+    } else if (!EmailValidator.validate(value)) {
       return "Please enter a valid email address";
     }
-
     return null;
   }
 
-  String? validatePassword(String? value) {
-    if (value!.isEmpty) {
+  String? validatePassword(String value) {
+    final hasUppercase = RegExp(r'[A-Z]').hasMatch(value);
+    final hasDigits = RegExp(r'[0-9]').hasMatch(value);
+    if (validateEmpty(value)) {
       return "Please enter a your password";
-    } else if(value.length < 6){
+    } else if (value.length < 6) {
       return "Password must have more than 5 symbols";
+    } else if (!hasUppercase) {
+      return "Password has at least one capital letter";
+    } else if (!hasDigits) {
+      return "Password has at least one digit";
     }
     return null;
+  }
+
+  String? validateConfirmPassword(String password, String confirmPassword) {
+    if (validateEmpty(confirmPassword)) {
+      return "Please confirm your password";
+    } else if (confirmPassword != password) {
+      return "Passwords do not match";
+    }
+    return null;
+  }
+
+  bool validateEmpty(String? value) {
+    if (value == null || value.isEmpty) {
+      return true;
+    }
+    return false;
   }
 }
