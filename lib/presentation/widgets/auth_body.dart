30600da0494e6b83ad7dd37f0f15cba9a7063e1d
Huong Truong <truongthihuong20020110@gmail.com>
2024-10-13 19:56:26 +0700

    features and screens of sign in and sign up

---
+++ lib/presentation/widgets/auth_body.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class CustomBody extends StatelessWidget {
+  const CustomBody(
+      {super.key,
+      required this.padding,
+      required this.marginTop,
+      required this.height});
+
+  final double marginTop;
+  final double height;
+  final Padding padding;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: marginTop),
+      height: height,
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(30),
+          topRight: Radius.circular(30),
+        ),
+      ),
+      child: padding,
+    );
+  }
+}
