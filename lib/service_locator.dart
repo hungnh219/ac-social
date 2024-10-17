2dd4666e8772f086dafafeeab64e2372d3aedcd2
Huong Truong <truongthihuong20020110@gmail.com>
2024-10-17 16:40:45 +0700

    splash and boarding update 17/10

--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -1,5 +1,4 @@
 import 'package:get_it/get_it.dart';
-import 'package:social_app/presentation/screens/auth/cubit/auth_cubit.dart';
 import 'data/repository/auth/auth_repository_impl.dart';
 import 'data/sources/auth/auth_firebase_service.dart';
 import 'domain/repository/auth/auth.dart';
@@ -7,6 +6,7 @@ import 'domain/repository/auth/auth.dart';
 final serviceLocator = GetIt.instance;
 
 Future<void> initializeDependencies() async {
-  serviceLocator.registerSingleton<AuthFirebaseService>(AuthFirebaseServiceImpl());
+  serviceLocator
+      .registerSingleton<AuthFirebaseService>(AuthFirebaseServiceImpl());
   serviceLocator.registerSingleton<AuthRepository>(AuthRepositoryImpl());
 }
